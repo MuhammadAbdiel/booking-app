0c8a270afec4981394ef20b1ac304107cf701471
MuhammadAbdiel <abdielfirdie@gmail.com>
2024-07-24 20:18:49 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./src/screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Details from "./src/screens/Details";
+import Booking from "./src/screens/Booking";
+import Checkout from "./src/screens/Checkout";
+import Success from "./src/screens/Success";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Booking" component={Booking} />
+        <Stack.Screen name="Checkout" component={Checkout} />
+        <Stack.Screen name="Success" component={Success} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
